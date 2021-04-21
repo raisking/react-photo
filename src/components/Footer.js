6e141bff44a5ecdfe7c8d0a51214750aaace4556
Prakash Rai <raiprakash@Prakashs-MBP.attlocal.net>
2021-04-21 10:56:27 -0400

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -64,11 +64,11 @@ function Footer() {
                 <div class='social-media-wrap'>
                     <div class='footer-logo'>
                         <Link to='/' className='social-logo'>
-                            TRVL
+                            TESTING`
               <i class='fab fa-typo3' />
                         </Link>
                     </div>
-                    <small class='website-rights'>TRVL © 2020</small>
+                    <small class='website-rights'>TESTING</small>
                     <div class='social-icons'>
                         <Link
                             class='social-icon-link facebook'
